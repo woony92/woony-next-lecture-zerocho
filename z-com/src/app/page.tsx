2e41e6a3863885ccef2cece10a270d70abb5cced
egChoi <2013agal@gmail.com>
2025-04-14 06:13:55 +0900

    2-5. css module을 선택한 이유

--- z-com/src/app/page.tsx
+++ z-com/src/app/page.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import zlogo from "../../public/zlogo.png";
+import styles from "./page.module.css";
+
 export default function App() {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.left}>
         <Image src={zlogo} alt="zogo" />
       </div>
-      <div>
+      <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요</h2>
-        <Link href="/i/flow/signup">계정 만들기</Link>
+        <Link href="/i/flow/signup" className={styles.signup}>
+          계정 만들기
+        </Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login">로그인</Link>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
       </div>
     </div>
   );
