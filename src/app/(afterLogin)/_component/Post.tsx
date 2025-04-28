257e2e9ef5279fd09a6a433066629d9aba6ddb4e
egChoi <2013agal@gmail.com>
2025-04-28 23:10:32 +0900

    3-12. faker.js와 /photo/[photoId]

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,5 +1,7 @@
-import ActionButtons from "@/app/(afterLogin)/_component/ActionButton";
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
+import { faker } from "@faker-js/faker";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -9,7 +11,10 @@ import style from "./Post.module.css";
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
-export default function Post() {
+type Props = {
+  noImage?: boolean;
+};
+export default function Post({ noImage }: Props) {
   const target = {
     postId: 1,
     User: {
@@ -19,8 +24,17 @@ export default function Post() {
     },
     content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() }
+    );
+  }
+
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
@@ -43,7 +57,9 @@ export default function Post() {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div>
+            <PostImages post={target} />
+          </div>
           <ActionButtons />
         </div>
       </div>
