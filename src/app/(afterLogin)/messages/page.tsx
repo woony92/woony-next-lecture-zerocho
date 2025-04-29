f8225af92558b018d5bc5a7b8a0033b76de84c46
egChoi <2013agal@gmail.com>
2025-04-29 19:21:03 +0900

    3-13. /messages 페이지 라이브 클론

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
+import Room from "@/app/(afterLogin)/messages/_component/Room";
+import style from "./messages.module.css";
+
 export default function Messages() {
-  return <div>Messages</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
